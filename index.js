65937d1fd0be82b4fa70c73e443595af99a1392f
education-bootcamp <education.seekerscloud@gmail.com>
2023-11-29 21:02:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+* express (npm i express)
+* mongoose (npm i mongoose)
+* nodemon (npm i nodemon -g (sudo))
+* dotenv (npm i dotenv)
+* body-parser (npm i body-parser)
+* bcrypt (npm i bcrypt)
+* jsonwebtoken (npm i jsonwebtoken)
+*
+* */
