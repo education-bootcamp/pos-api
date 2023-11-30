b2316d891be5760afc2955789a9854b906339b59
education-bootcamp <education.seekerscloud@gmail.com>
2023-11-30 19:08:55 +0530

    6

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,6 +2,7 @@ const userSchema= require('../model/UserSchema');
 const bcrypt= require('bcrypt');
 const salt=10;
 const nodemailer= require('nodemailer');
+const jsonwebtoken =require('jsonwebtoken');
 
 const register = (req,resp) => {
 
@@ -53,7 +54,31 @@ const register = (req,resp) => {
 }
 
 const login = (req,resp) => {
+    userSchema.findOne({'email':req.body.email}).then(selectedUser=>{
+        if (selectedUser!==null){
+            bcrypt.compare(req.body.password, selectedUser.password, function(err, result) {
+               if (err){
+                   return resp.status(500).json({'message':'internal server error'});
+               }
 
+               if(result){
+                   const payload={
+                       email:selectedUser.email
+                   }
+
+                   const secretKey=process.env.SECRET_KEY;
+                   const expiresIn='24h';
+
+                   const token = jsonwebtoken.sign(payload,secretKey,{expiresIn});
+                   return resp.status(200).json({'token':token});
+               }else{
+                   return resp.status(401).json({'message':'Password is incorrect!'});
+               }
+            });
+        }else{
+            return resp.status(404).json({'message':'not found!'});
+        }
+    });
 }
 
 module.exports={
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -2,4 +2,5 @@ const express = require('express');
 const userController = require('../controller/UserController');
 const router = express.Router();
 router.post('/register', userController.register);
+router.post('/login', userController.login);
 module.exports=router;
